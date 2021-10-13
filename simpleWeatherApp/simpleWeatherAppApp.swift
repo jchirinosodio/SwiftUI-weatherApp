7aed36b4b91ed7247ce8bd5220596bc8d3e9f189
Jose Eduardo Chirinos Odio <jchirinosodio@gmail.com>
2021-10-13 13:33:04 -0600

    Initial Commit

---
+++ simpleWeatherApp/simpleWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleWeatherAppApp.swift
+//  simpleWeatherApp
+//
+//  Created by Jose Chirinos Odio on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct simpleWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
